8be6c7aadbda650b3d438c93465f6ed73fffa347
ffuertes1 <ffuertes25@gmail.com>
2024-08-06 14:07:18 -0700

    While Statement Example

---
+++ whileStatement.py
@@ -0,0 +1,6 @@
+#Example of the while statement. The while statement will iterate the loop until it reaches 6, this means it will go out of the loop and finish.
+variable= 0
+while variable <5:
+    print('Hello, variable its less than 5 ')
+    variable= variable + 1
+print(variable)
