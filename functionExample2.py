fe5f27a740d1254048980e0bed8130be11b43d22
ffuertes1 <ffuertes25@gmail.com>
2024-08-07 15:09:31 -0700

    Chapter 3

---
+++ functionExample2.py
@@ -0,0 +1,6 @@
+#This case we will not use an input from the user, just hardcode the function
+# name=input() we can pass a comman input as parameter and it will work
+def hello(name):
+    print('hello '+ name)
+
+hello('Alanis')
---
+++ functionsExample1.py
@@ -0,0 +1,8 @@
+def hello():
+    print('Hello!')
+    print('HELLOOOO!')
+    print('hellooooowwww there!!!')
+
+hello()
+hello()
+hello()
