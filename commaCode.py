49b3f29776b5115254d4220b0d17360c7097e043
ffuertes1 <ffuertes25@gmail.com>
2024-08-12 10:30:38 -0700

    Chapter 4 last Assignment page(EMPTY)

--- commaCode.py
+++ commaCode.py
@@ -1,16 +1,2 @@
-
-print('Please enter a list of something: ')
-
-listStr=[]
-listStr=input()
-
-def commaCode(listStr):
-    if listStr==[]:
-        print('List is empty')
-        return listStr
-            
-        
-
-
-
-commaCode(listStr)
+#Empty chapter 4 last page
+# Try it later
