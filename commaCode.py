f23f982142d66324e0c19dbd8f4401bcc304def4
ffuertes1 <ffuertes25@gmail.com>
2024-08-12 10:28:57 -0700

    Chapter 4

---
+++ commaCode.py
@@ -0,0 +1,16 @@
+
+print('Please enter a list of something: ')
+
+listStr=[]
+listStr=input()
+
+def commaCode(listStr):
+    if listStr==[]:
+        print('List is empty')
+        return listStr
+            
+        
+
+
+
+commaCode(listStr)
---
+++ passingReference.py
@@ -0,0 +1,5 @@
+spam=[2,3,4,5,6,7,8]
+
+spam[2]='Hello'
+
+print(spam)
